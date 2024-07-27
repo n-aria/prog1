0e0db6c4128c6f367e395a647da6f59a2ac171c4
Justin Nguyen <justinnguyen@justins-mbp.lan>
2024-07-26 20:42:31 -0400

    added decoded func

--- NMA_Lab9.py
+++ NMA_Lab9.py
@@ -4,3 +4,10 @@ def encode(password):
         new_digit = (int(char) + 3) % 10
         new_pass += str(new_digit)
     return new_pass
+
+def decode(password):
+    decoded_pass = ''
+    for char in password:
+        new_digit = (int(char) - 3) % 10
+        decoded_pass += str(new_digit)
+    return decoded_pass
